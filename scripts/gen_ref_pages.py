5f74f2852de92d2b45ca7ff47efeb216b4536af1
ZeroBone <zerobone21@gmail.com>
2024-01-11 00:37:11 +0100

    Fixed documentation build errors

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -1,15 +1,15 @@
-"""Generate the code reference pages and navigation."""
+"""Generate the API reference pages and navigation."""
 
 from pathlib import Path
 
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
-mod_symbol = '<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code>'
 
 src = Path(__file__).parent.parent / "src"
 
 for path in sorted(src.rglob("*.py")):
+
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
@@ -23,14 +23,13 @@ for path in sorted(src.rglob("*.py")):
     elif parts[-1] == "__main__":
         continue
 
-    nav_parts = [f"{mod_symbol} {part}" for part in parts]
-    nav[tuple(nav_parts)] = doc_path.as_posix()
+    nav[tuple(parts)] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
         fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path)
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 with mkdocs_gen_files.open("reference/summary.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
