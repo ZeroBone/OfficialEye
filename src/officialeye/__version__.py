e513f2a5204e4cdee27125e27060c28258378415
ZeroBone <zerobone21@gmail.com>
2024-01-22 16:56:17 +0100

    Version 1.1.5

--- src/officialeye/__version__.py
+++ src/officialeye/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "officialeye"
 __description__ = "An AI-powered generic document-analysis tool."
 __url__ = "https://officialeye.zerobone.net"
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 __author__ = "Alexander Mayorov"
 __author_email__ = "zb@zerobone.net"
 __license__ = "GPL-3.0"
