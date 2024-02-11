da1765165f03bb43360a73511a245f93a297ceb9
ZeroBone <zerobone21@gmail.com>
2024-02-11 20:51:54 +0100

    Fixed a unit test failure.

--- src/officialeye/detection.py
+++ src/officialeye/detection.py
@@ -2,4 +2,8 @@
 Module providing an API for all OfficialEye's document detection tools.
 """
 
+# disable unused imports ruff check
+# ruff: noqa: F401
+
 # noinspection PyProtectedMember
+from officialeye._api.detection import detect
