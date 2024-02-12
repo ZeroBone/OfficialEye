203ea80ddd48899795d384cf0bcc98a103c2e472
ZeroBone <zerobone21@gmail.com>
2024-02-12 20:01:30 +0100

    Improved a unit test.

--- src/tests/unit/api/test_detect.py
+++ src/tests/unit/api/test_detect.py
@@ -1,8 +1,14 @@
-from officialeye import Context, IImage, Image, ISupervisionResult, Template
+from difflib import SequenceMatcher
+
+from officialeye import Context, IImage, Image, ISupervisionResult, Template, IInterpretationResult
 from officialeye.detection import detect
 
 
-def test_detect():
+def sequence_similarity_measure(str_1: str, str_2: str, /) -> float:
+    return SequenceMatcher(None, str_1, str_2).ratio()
+
+
+def test_driver_license_ru():
 
     with Context() as context:
         template = Template(context, path="docs/assets/templates/driver_license_ru_01/driver_license_ru.yml")
@@ -13,3 +19,16 @@ def test_detect():
         result = detect(context, template, target=image)
 
         assert isinstance(result, ISupervisionResult)
+        assert result.template.identifier == "driver_license_ru"
+
+        interpretation: IInterpretationResult = result.interpret(target=image)
+        assert isinstance(interpretation, IInterpretationResult)
+
+        feature_interpretation_dict = {}
+
+        for feature in result.template.features:
+            feature_interpretation_dict[feature.identifier] = interpretation.get_feature_interpretation(feature)
+
+        assert sequence_similarity_measure(feature_interpretation_dict["last_name_ru"], "СУРГУТСКИЙ") >= 0.6
+        assert sequence_similarity_measure(feature_interpretation_dict["name_ru"], "ИГОРЬ ВЛАДИСЛАВОВИЧ") >= 0.6
+        assert sequence_similarity_measure(feature_interpretation_dict["birthday"], "16.10.1986") >= 0.8
